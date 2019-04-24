743c6b6e25086290a498347c1c36b82c92db744d
chulhee23 <chulhee23@naver.com>
2019-04-24 15:30:26 +0900

    challenging...

--- algorithm/dynamic_programming/10844.py
+++ algorithm/dynamic_programming/10844.py
@@ -1,21 +1,44 @@
+	
+# import sys
+ 
+# num = int(sys.stdin.readline())
+ 
+# arr = [1] * 10
+# arr[0] = 0
+ 
+# print(arr)
+# for i in range(1, num):
+#     new_arr = [0]*10
+#     for (j, e) in enumerate(arr):
+#         if j == 0:
+#             new_arr[1] += e
+#         elif j == 9:
+#             new_arr[8] += e
+#         else:
+#             new_arr[j-1] += e
+#             new_arr[j+1] += e
+ 
+#     arr = new_arr
+ 
+# print(arr)
+# sys.stdout.write(str(sum(arr)%1000000000))
+
 import sys
-input = sys.stdin.readline
+arr = [1] * 10
+arr[0] = 0
 
-digit = int(input()) # 몇자리 수인지 입력
-
-ans=[0,9,17]
-
-units=[0,1,2,3,4,5,6,7,8,9]
-
-number=123
-
-unit= number%10
-
-if unit==0:
-#0은 1을 끝에 갖는다
-
-elif (unit>0 and unit<9):
-# 1~8은 2개의 가짓수 가질수 있음
-
-else:
-# 9는 한가지 생성
+num=3
+ 
+for i in range(1, num):
+    new_arr = [0]*10
+    for (j, e) in enumerate(arr):
+        if j == 0:
+            new_arr[1] += e
+        elif j == 9:
+            new_arr[8] += e
+        else:
+            new_arr[j-1] += e
+            new_arr[j+1] += e
+ 
+    arr = new_arr
+sys.stdout.write(str(sum(arr)%1000000000))
