681794aa0c58b22705f0a9bc1af0d7da3ac9e62a
chulhee23 <chulhee23@naver.com>
2019-04-24 17:16:25 +0900

    이친수 성공

--- algorithm/dynamic_programming/2193.py
+++ algorithm/dynamic_programming/2193.py
@@ -1,6 +1,15 @@
 import sys
 n= int(sys.stdin.readline())
 
-dp=[[0]*2 for i in range(n)]
-print(dp)
+dp=[[0]*2 for i in range(n+1)]
+dp[1][1]=1
+
+for i in range(0,n+1):
+    if (i<2):
+        continue
+    for j in range(2):
+        dp[i][0] = dp[i-1][0] + dp[i-1][1]
+        dp[i][1] = dp[i-1][0]
+
+print(sum(dp[n]))
 
