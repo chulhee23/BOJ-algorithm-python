388c78c81f1865347a5d13a229263c909c39da82
chulhee23 <chulhee23@naver.com>
2019-04-25 22:09:07 +0900

    stack의 느낌이 온다....

--- algorithm/dynamic_programming/9465.py
+++ algorithm/dynamic_programming/9465.py
@@ -1,9 +1,21 @@
 import sys
 input = sys.stdin.readline
 ans=[]
+
+
 test_cases_number = int(input())
 for _ in range(test_cases_number):
+    matrix = []
     n = int(input())
-    
+    # create matrix
+    matrix.append(list(map(int, input().split())))
+    matrix.append(list(map(int, input().split())))
 
-    ans.append()
+
+    # print answer form like this way
+    temp=1
+
+    ans.append(temp)
+
+print(matrix)
+print(*ans, end=" ")
