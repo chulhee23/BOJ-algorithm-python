a091c88fc72e79b802e52bde42daafe257e533dd
chulhee23 <chulhee23@naver.com>
2019-04-25 22:04:19 +0900

    matrix input setting

---
+++ algorithm/dynamic_programming/9465.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+ans=[]
+test_cases_number = int(input())
+for _ in range(test_cases_number):
+    n = int(input())
+    
+
+    ans.append()
--- skill/matrix.py
+++ skill/matrix.py
@@ -4,3 +4,8 @@ n= int(sys.stdin.readline())
 dp=[[0]*2 for i in range(n)]
 print(dp)
 
+col, row = map(int, input().split())
+matrix = []
+
+for i in range(col):
+    matrix.append(list(map(int, input().split())))
