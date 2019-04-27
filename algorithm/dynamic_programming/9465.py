577174704b4f2f105ec572c339aecc9c6a044ba4
chulhee23 <chulhee23@naver.com>
2019-04-27 11:12:04 +0900

    참패...

--- algorithm/dynamic_programming/9465.py
+++ algorithm/dynamic_programming/9465.py
@@ -11,6 +11,7 @@ for _ in range(test_cases_number):
     matrix.append(list(map(int, input().split())))
     matrix.append(list(map(int, input().split())))
 
+    # 점화식의 필연성을 고려할 것.
 
     # print answer form like this way
     temp=1
