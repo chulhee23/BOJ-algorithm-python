387ab38579b4e67e25cd6a28dfd280e562d44b4e
chulhee23 <chulhee23@naver.com>
2019-05-22 14:22:32 +0900

    runtime error

--- algorithm/dynamic_programming/9465.py
+++ algorithm/dynamic_programming/9465.py
@@ -1,20 +1,22 @@
 import sys
-input = sys.stdin.readline
 
-repeat = int(input())
+
+# repeat = int(input())
 
 # 1회에 대한 알고리즘
-col = map(int, input().split())
+col = int(sys.stdin.readline())
 sticker = []
 
-for _ in range(col):
+for i in range(2):
     sticker.append(list(map(int, input().split())))
+    sticker[i].insert(0,0)
+    sticker[i].insert(0,0)
 
-sticker[0].insert(0,0)
-sticker[1].insert(0,0)
-dp=[[0]*(col+1) for i in range(2)]
+print(sticker)
+dp=[[0]*(col+2) for i in range(2)]
 
-for i in range(2,col+1):
+for i in range(2,col+2):
     dp[0][i]=max(dp[1][i-1],dp[1][i-2]) + sticker[0][i]
     dp[1][i]=max(dp[0][i-1],dp[0][i-2]) + sticker[1][i]
 
+print(max(dp[0][col+1],dp[1][col+1]))
