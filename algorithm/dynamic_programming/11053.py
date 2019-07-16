d0215b6e9f6a615d67d8d9878ebc80b6028ef868
chulhee23 <chulhee23@naver.com>
2019-07-16 21:00:54 +0900

    11053 복습해야함

--- algorithm/dynamic_programming/11053.py
+++ algorithm/dynamic_programming/11053.py
@@ -12,12 +12,21 @@
 # 출력
 # 첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
 
+
+# 전체를 다시 탐색해도 상관 없음 어차피 n^2
 import sys
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
-size = int(input())
-
+n = int(input())
 arr = list(map(int, input().split()))
-
-
-
+d = [0]*n
+for i in range(0, n):
+    if i == 0:
+        d[i] = 1
+    else:
+        m = 0
+        for j in range(i):
+            if (arr[j] < arr[i]) and (m < d[j]):
+                m = d[j]
+        d[i] = m + 1
+print(max(d))
