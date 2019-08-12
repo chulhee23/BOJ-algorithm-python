0f07686c38a37bc6efc177577ac18a9dd0cf2420
chulhee23 <chulhee23@naver.com>
2019-08-12 23:54:37 +0900

    merge sort

---
+++ algorithm/etc/2751.py
@@ -0,0 +1,40 @@
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+import sys
+input = sys.stdin.readline
+
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list) // 2
+    leftList = list[:mid]
+    rightList = list[mid:]
+    leftList = merge_sort(leftList)
+    rightList = merge_sort(rightList)
+    return merge(leftList, rightList)
+
+def merge(left, right):
+    result = []
+    while len(left) > 0 or len(right) > 0:
+        if len(left) > 0 and len(right) > 0:
+            if left[0] <= right[0]:
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
+        elif len(left) > 0:
+            result.append(left[0])
+            left = left[1:]
+        elif len(right) > 0:
+            result.append(right[0])
+            right = right[1:]
+    return result
+
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+arr = merge_sort(arr)
+print(arr)
