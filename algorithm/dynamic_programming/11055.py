e29b82a742bd3159c77998dd78924c1b7686b3d5
chulhee23 <chulhee23@naver.com>
2019-08-18 23:27:01 +0900

    runtime 해결

--- algorithm/dynamic_programming/11055.py
+++ algorithm/dynamic_programming/11055.py
@@ -2,14 +2,18 @@
  # 현재 비교기준 값(d[i])이 이전번째까지의 합(d[j]) + 자신의 값(a[i])보다 작을 때, 
  # d[i] = d[j] + a[i]를 해준다
 import sys
-n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-
+ 
+num = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().rstrip().split()))
+ 
+d = [0 for i in range(num)]
+ 
 for i in range(num):
     d[i] = arr[i]
-for i in range(n):
+ 
+for i in range(num):
     for j in range(i, -1, -1):
-        if arr[j] < arr[i] and d[i] < d[j] + arr[i]:
-            d[i] = arr[i] +d[j]
-
-print(d)
+        if arr[j] < arr[i] and d[j] > d[i] - arr[i]:
+            d[i] = d[j] + arr[i]
+ 
+sys.stdout.write(str(max(d)))
